33c193a890f5ecbfe2007e9628fb56c00856e545
ArchitWa <awankhade404@student.fuhsd.org>
2022-01-31 12:31:09 -0800

    Intial answer filter

---
+++ src/AnswerFilter.java
@@ -0,0 +1,7 @@
+import core.DImage;
+
+public class AnswerFilter {
+    public static String[] processImage(DImage image) {
+        return null;
+    }
+}
--- src/OpticalMarkReaderMain.java
+++ src/OpticalMarkReaderMain.java
@@ -1,37 +1,38 @@
-import FileIO.PDFHelper;
-import core.DImage;
-import processing.core.PImage;
+    import FileIO.PDFHelper;
+    import core.DImage;
+    import processing.core.PImage;
 
-import javax.swing.*;
-import java.io.File;
-import java.util.ArrayList;
+    import javax.swing.*;
+    import java.io.File;
+    import java.util.ArrayList;
 
-public class OpticalMarkReaderMain {
-    public static void main(String[] args) {
-        String pathToPdf = fileChooser();
-        System.out.println("Loading pdf at " + pathToPdf);
-        ArrayList<PImage> pdf = PDFHelper.getPImagesFromPdf(pathToPdf);
-        ArrayList<String[]> answers = new ArrayList<>();
-        for (PImage page : pdf) {
-            DImage image = new DImage(page);
-            //// run image in filter
+    public class OpticalMarkReaderMain {
+        public static void main(String[] args) {
+            String pathToPdf = fileChooser();
+            System.out.println("Loading pdf at " + pathToPdf);
+
+            ArrayList<PImage> pdf = PDFHelper.getPImagesFromPdf(pathToPdf);
+            ArrayList<String[]> answers = new ArrayList<>();
+            for (PImage page : pdf) {
+                DImage image = new DImage(page);
+                String[] answer = AnswerFilter.processImage(image);
+            }
+            /*
+            Your code here to...
+            (1).  Load the pdf
+            (2).  Loop over its pages
+            (3).  Create a DImage from each page and process its pixels
+            (4).  Output 2 csv files
+             */
+
         }
-        /*
-        Your code here to...
-        (1).  Load the pdf
-        (2).  Loop over its pages
-        (3).  Create a DImage from each page and process its pixels
-        (4).  Output 2 csv files
-         */
 
+        private static String fileChooser() {
+            String userDirLocation = System.getProperty("user.dir");
+            File userDir = new File(userDirLocation);
+            JFileChooser fc = new JFileChooser(userDir);
+            int returnVal = fc.showOpenDialog(null);
+            File file = fc.getSelectedFile();
+            return file.getAbsolutePath();
+        }
     }
-
-    private static String fileChooser() {
-        String userDirLocation = System.getProperty("user.dir");
-        File userDir = new File(userDirLocation);
-        JFileChooser fc = new JFileChooser(userDir);
-        int returnVal = fc.showOpenDialog(null);
-        File file = fc.getSelectedFile();
-        return file.getAbsolutePath();
-    }
-}
