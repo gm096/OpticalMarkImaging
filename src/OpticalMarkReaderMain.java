3029030f5ae42825b5b4e9fbdecaa775b279ec55
ArchitWa <98776209+ArchitWa@users.noreply.github.com>
2022-02-07 13:00:37 -0800

    Update OpticalMarkReaderMain.java

--- src/OpticalMarkReaderMain.java
+++ src/OpticalMarkReaderMain.java
@@ -1,6 +1,7 @@
 import FileIO.PDFHelper;
 import Filters.AnswerFilter;
 import core.DImage;
+import jogamp.graph.font.typecast.ot.table.ID;
 import processing.core.PImage;
 
 import javax.swing.*;
@@ -11,55 +12,55 @@ import java.util.ArrayList;
 
 public class OpticalMarkReaderMain {
     public static void main(String[] args) {
-        String pathToPdf = fileChooser();
+        String pathToPdf = "assets/omrtest.pdf";
         System.out.println("Loading pdf at " + pathToPdf);
 
         ArrayList<PImage> pdf = PDFHelper.getPImagesFromPdf(pathToPdf);
         ArrayList<ArrayList<String>> allAnswers = new ArrayList<>();
+        ArrayList<ArrayList<String>> allIDs = new ArrayList<>();
+
         for (PImage page : pdf) {
             DImage image = new DImage(page);
             ArrayList<String> answers = new ArrayList<>();
+            ArrayList<String> IDs = new ArrayList<>();
 
             AnswerFilter filter = new AnswerFilter();
-            filter.processImage(image, answers);
+            filter.processImage(image, answers, IDs);
             allAnswers.add(answers);
-    }
+            allIDs.add(IDs);
+        }
         ArrayList<String> answerKey = allAnswers.get(0);
-        outputCSV(allAnswers, answerKey);
+        outputCSV(allAnswers, allIDs, answerKey);
     }
 
-    private static void outputCSV(ArrayList<ArrayList<String>> allAnswers, ArrayList<String> answerKey) {
+    private static void outputCSV(ArrayList<ArrayList<String>> allAnswers, ArrayList<ArrayList<String>> allIDs, ArrayList<String> answerKey) {
         ArrayList<ArrayList<Boolean>> testGrades = new ArrayList<>();
         for (int i = 0; i < allAnswers.size(); i++) {
             ArrayList<Boolean> testGrade = new ArrayList<>();
-            for (int j = 0; j < allAnswers.get(i).size(); j++) {
-                if(allAnswers.get(i).get(j).equals(answerKey.get(j))){
-                    testGrade.add(true);
-                }
-                else{
-                    testGrade.add(false);
-                }
+            ArrayList<String> currPage = allAnswers.get(i);
+
+            for (int j = 0; j < currPage.size(); j++) {
+                if (currPage.get(j).equals(answerKey.get(j))) testGrade.add(true);
+                else testGrade.add(false);
             }
+
             testGrades.add(testGrade);
         }
-        try{
+        try {
             PrintWriter out = new PrintWriter(new FileWriter("answers.csv"));
-            for (ArrayList<Boolean> testGrade:testGrades) {
+            for (ArrayList<Boolean> testGrade : testGrades) {
                 int counter = 0;
-                for (Boolean answer:
-                     testGrade) {
-                    if(answer == true){
-                        counter++;
-                        out.print(answer + ", ");
-                    }
-                    else{
-                        out.print(answer + ", ");
-                    }
+                int IDCounter = 0;
+                out.println("Student ID: " + allIDs.get(IDCounter).get(0) + " Teacher ID: " + allIDs.get(IDCounter).get(1) + " ");
+                IDCounter++;
+                for (Boolean answer : testGrade) {
+                    if (answer) counter++;
+                    out.print(answer + ", ");
                 }
-                out.println(counter + "/" + testGrade.size() + ", " + (double)counter/testGrade.size());
+                out.println(counter + "/" + testGrade.size() + ", " + (((double) counter / testGrade.size()) * 100) + "%");
             }
             out.close();
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         outputItemAnalysis(testGrades);
@@ -69,18 +70,18 @@ public class OpticalMarkReaderMain {
         int[] itemAnalysis = new int[testGrades.get(0).size()];
         for (int i = 1; i < testGrades.size(); i++) {
             for (int j = 0; j < testGrades.get(i).size(); j++) {
-                if(testGrades.get(i).get(j) == false){
+                if (testGrades.get(i).get(j) == false) {
                     itemAnalysis[j]++;
                 }
             }
         }
-        try{
+        try {
             PrintWriter out = new PrintWriter(new FileWriter("itemanalysis.csv"));
             for (int i = 0; i < itemAnalysis.length; i++) {
                 out.println(i + ", " + itemAnalysis[i]);
             }
             out.close();
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
