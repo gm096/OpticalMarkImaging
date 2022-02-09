3d10d6cdcb5a90ec385e1fdb2a0a80861a3f6fee
ArchitWa <98776209+ArchitWa@users.noreply.github.com>
2022-02-09 11:02:54 -0800

    Update DAnswerFilter.java

--- src/Filters/DAnswerFilter.java
+++ src/Filters/DAnswerFilter.java
@@ -13,8 +13,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DAnswerFilter implements PixelFilter, Interactive, Drawable {
-    private final int cRadius = 9, dist = (2 * cRadius) + 20; // radius of bubbles and space between each bubble
-    private int xODist, startingX, startingY;
+    private int cRadius = 9; // radius of bubbles
+    private int xODist, yDist, startingX, startingY;
     private int rows, columns, answers;
     private ArrayList<Location> corners;
 
@@ -22,11 +22,11 @@ public class DAnswerFilter implements PixelFilter, Interactive, Drawable {
         corners = new ArrayList<>();
 
         Scanner kb = new Scanner(System.in);
-        System.out.println("Supply the number of rows and columns and the number of bubbles per question, separated by a space:");
+        System.out.println("Supply the number of columns and rows and the number of bubbles per question, separated by a space:");
         String input = kb.nextLine();
 
-        rows = Integer.parseInt(input.split(" ")[0]);
-        columns = Integer.parseInt(input.split(" ")[1]);
+        columns = Integer.parseInt(input.split(" ")[0]);
+        rows = Integer.parseInt(input.split(" ")[1]);
         answers = Integer.parseInt(input.split(" ")[2]);
     }
 
@@ -47,8 +47,8 @@ public class DAnswerFilter implements PixelFilter, Interactive, Drawable {
     }
 
     private void boxImage(short[][][] cChannels) {
-        for (int c = startingX / 2; c < (rows * xODist / 2); c += xODist / 2) {
-            for (int r = startingY / 2; r < startingY / 2 + columns * (dist) / 2; r += dist / 2) {
+        for (int c = startingX / 2; c < (columns * xODist / 2); c += xODist / 2) {
+            for (int r = startingY / 2; r < startingY / 2 + rows * (yDist) / 2; r += yDist / 2) {
                 makeDBox(cChannels, r, c);
             }
         }
@@ -57,14 +57,14 @@ public class DAnswerFilter implements PixelFilter, Interactive, Drawable {
     private void makeDBox(short[][][] iChannels, int row, int col) {
         int[] boxColor = {255, 0, 0};
         for (int c = 0; c < boxColor.length; c++) {
-            for (int i = row; i <= row + dist / 2; i++) {
-                iChannels[c][i][col] = (short) boxColor[c];
-                iChannels[c][i][col + xODist / 2] = (short) boxColor[c];
+            for (int i = row; i <= row + yDist / 2; i++) {
+                if (inBound(iChannels[0], i, col))   iChannels[c][i][col] = (short) boxColor[c];
+                if (inBound(iChannels[0], i, col + xODist / 2))  iChannels[c][i][col + xODist / 2] = (short) boxColor[c];
             }
 
             for (int i = col; i <= col + xODist / 2; i++) {
-                iChannels[c][row][i] = (short) boxColor[c];
-                iChannels[c][row + dist / 2][i] = (short) boxColor[c];
+                if (inBound(iChannels[0], row, i)) iChannels[c][row][i] = (short) boxColor[c];
+                if (inBound(iChannels[0], row + yDist / 2, i)) iChannels[c][row + yDist / 2][i] = (short) boxColor[c];
             }
         }
     }
@@ -85,6 +85,10 @@ public class DAnswerFilter implements PixelFilter, Interactive, Drawable {
         }
     }
 
+    public static boolean inBound(short[][] arr, int r, int c) {
+        return (arr.length - 1 >= r && arr[0].length - 1 >= c && r >= 0 && c >= 0);
+    }
+
     @Override
     public void mouseClicked(int mouseX, int mouseY, DImage img) {
         corners.add(new Location(mouseX, mouseY));
@@ -100,7 +104,7 @@ public class DAnswerFilter implements PixelFilter, Interactive, Drawable {
         try {
             PrintWriter out = new PrintWriter(new FileWriter("format.csv"));
 
-            out.println(rows + " " + columns + " " + startingX + " " + startingY + " " + xODist + " " + answers);
+            out.println(columns + " " + rows + " " + startingX + " " + startingY + " " + xODist + " " + answers);
 
             out.close();
         } catch (Exception e) {
@@ -116,6 +120,7 @@ public class DAnswerFilter implements PixelFilter, Interactive, Drawable {
             startingX = l1.x * 2;
             startingY = l1.y * 2;
             xODist = (l2.x - l1.x) * 2;
+            yDist = (l2.y - l1.y) * 2;
         }
     }
 }
