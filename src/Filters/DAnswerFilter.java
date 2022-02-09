0657066c9bffb2a0c15e6cfeb29a0d4dd2d464ec
ArchitWa <98776209+ArchitWa@users.noreply.github.com>
2022-02-08 22:31:07 -0800

    Added an interactive way to set the boxes
    The r key is used to reset the boxes so that you can retry making the formatting
    The c key is to save the changes to a format.csv which will be used later on

--- src/Filters/DAnswerFilter.java
+++ src/Filters/DAnswerFilter.java
@@ -1,44 +1,55 @@
 package Filters;
 
+import FileIO.Location;
+import Interfaces.Drawable;
 import Interfaces.Interactive;
 import Interfaces.PixelFilter;
 import core.DImage;
+import processing.core.PApplet;
 
-public class DAnswerFilter implements PixelFilter, Interactive {
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DAnswerFilter implements PixelFilter, Interactive, Drawable {
     private final int cRadius = 9, dist = (2 * cRadius) + 20; // radius of bubbles and space between each bubble
-    private final int xODist = 281; // dimensions of outer box
-    private int startingX = 0, startingY = 0;
+    private int xODist, startingX, startingY;
+    private int rows, columns, answers;
+    private ArrayList<Location> corners;
 
+    public DAnswerFilter() {
+        corners = new ArrayList<>();
+
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Supply the number of rows and columns and the number of bubbles per question, separated by a space:");
+        String input = kb.nextLine();
+
+        rows = Integer.parseInt(input.split(" ")[0]);
+        columns = Integer.parseInt(input.split(" ")[1]);
+        answers = Integer.parseInt(input.split(" ")[2]);
+    }
 
     public DImage processImage(DImage img) {
         short[][] pixels = img.getBWPixelGrid();
         short[][] outputPixels = new short[pixels.length / 2][pixels[0].length / 2];
-        short[][][] cChannels = {new short[outputPixels.length][outputPixels[0].length], new short[outputPixels.length][outputPixels[0].length], new short[outputPixels.length][outputPixels[0].length]};
+        short[][][] iChannels = {img.getRedChannel(), img.getGreenChannel(), img.getBlueChannel()};
+        short[][][] oChannels = {new short[outputPixels.length][outputPixels[0].length], new short[outputPixels.length][outputPixels[0].length], new short[outputPixels.length][outputPixels[0].length]};
 
 
-        downsampleImage(pixels, outputPixels);
-        boxImage(outputPixels, cChannels);
+        downsampleImage(iChannels, oChannels);
+        if (corners.size() >= 2) boxImage(oChannels);
 
         DImage nImg = new DImage(outputPixels[0].length, outputPixels.length);
         nImg.setPixels(outputPixels);
-        nImg.setColorChannels(cChannels[0], cChannels[1], cChannels[2]);
+        nImg.setColorChannels(oChannels[0], oChannels[1], oChannels[2]);
         return nImg;
     }
 
-    private void boxImage(short[][] outputPixels, short[][][] cChannels) {
-        for (int c = startingX / 2; c < (4 * xODist / 2); c += xODist / 2) {
-            for (int r = startingY / 2; r < startingY / 2 + 25 * (dist) / 2; r += dist / 2) {
+    private void boxImage(short[][][] cChannels) {
+        for (int c = startingX / 2; c < (rows * xODist / 2); c += xODist / 2) {
+            for (int r = startingY / 2; r < startingY / 2 + columns * (dist) / 2; r += dist / 2) {
                 makeDBox(cChannels, r, c);
-            }
-        }
-
-        for (int r = 0; r < outputPixels.length; r++) {
-            for (int c = 0; c < outputPixels[0].length; c++) {
-                if (cChannels[0][r][c] == 0) {
-                    cChannels[0][r][c] = outputPixels[r][c];
-                    cChannels[1][r][c] = outputPixels[r][c];
-                    cChannels[2][r][c] = outputPixels[r][c];
-                }
             }
         }
     }
@@ -47,24 +58,26 @@ public class DAnswerFilter implements PixelFilter, Interactive {
         int[] boxColor = {255, 0, 0};
         for (int c = 0; c < boxColor.length; c++) {
             for (int i = row; i <= row + dist / 2; i++) {
-                if (inBound(iChannels[0], i, col)) iChannels[c][i][col] = (short) boxColor[c];
-                if (inBound(iChannels[0], i, col + xODist / 2)) iChannels[c][i][col + xODist / 2] = (short) boxColor[c];
+                iChannels[c][i][col] = (short) boxColor[c];
+                iChannels[c][i][col + xODist / 2] = (short) boxColor[c];
             }
 
             for (int i = col; i <= col + xODist / 2; i++) {
-                if (inBound(iChannels[0], row, i)) iChannels[c][row][i] = (short) boxColor[c];
-                if (inBound(iChannels[0], row + dist / 2, i)) iChannels[c][row + dist / 2][i] = (short) boxColor[c];
+                iChannels[c][row][i] = (short) boxColor[c];
+                iChannels[c][row + dist / 2][i] = (short) boxColor[c];
             }
         }
     }
 
-    private void downsampleImage(short[][] pixels, short[][] outputPixels) {
+    private void downsampleImage(short[][][] iChannels, short[][][] oChannels) {
         int rc = 0;
         int cc = 0;
 
-        for (int r = 0; r < pixels.length - 1; r += 2) {
-            for (int c = 0; c < pixels[r].length - 1; c += 2) {
-                outputPixels[rc][cc] = pixels[r][c];
+        for (int r = 0; r < iChannels[0].length - 1; r += 2) {
+            for (int c = 0; c < iChannels[0][r].length - 1; c += 2) {
+                oChannels[0][rc][cc] = iChannels[0][r][c];
+                oChannels[1][rc][cc] = iChannels[1][r][c];
+                oChannels[2][rc][cc] = iChannels[2][r][c];
                 cc++;
             }
             rc++;
@@ -72,19 +85,37 @@ public class DAnswerFilter implements PixelFilter, Interactive {
         }
     }
 
-    public static boolean inBound(short[][] arr, int r, int c) {
-        return (arr.length - 1 >= r && arr[0].length - 1 >= c && r >= 0 && c >= 0);
-    }
-
     @Override
     public void mouseClicked(int mouseX, int mouseY, DImage img) {
-        startingX = mouseX * 2;
-        startingY = mouseY * 2;
-        processImage(img);
+        corners.add(new Location(mouseX, mouseY));
     }
 
     @Override
     public void keyPressed(char key) {
+        if (key == 'r') corners.clear();
+        if (key == 'c') saveToCSV();
+    }
 
+    private void saveToCSV() {
+        try {
+            PrintWriter out = new PrintWriter(new FileWriter("format.csv"));
+
+            out.println(rows + " " + columns + " " + startingX + " " + startingY + " " + xODist + " " + answers);
+
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void drawOverlay(PApplet window, DImage original, DImage filtered) {
+        if (corners.size() >= 2) {
+            Location l1 = corners.get(0), l2 = corners.get(1);
+
+            startingX = l1.x * 2;
+            startingY = l1.y * 2;
+            xODist = (l2.x - l1.x) * 2;
+        }
     }
 }
