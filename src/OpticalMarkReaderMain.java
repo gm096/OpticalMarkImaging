084b7b225c723990af081a31de31cdf75224f139
ArchitWa <98776209+ArchitWa@users.noreply.github.com>
2022-02-09 21:04:54 -0800

    Updated code so that it takes a csv for formatting and can be interactive

--- src/OpticalMarkReaderMain.java
+++ src/OpticalMarkReaderMain.java
@@ -4,27 +4,31 @@ import core.DImage;
 import processing.core.PImage;
 
 import javax.swing.*;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class OpticalMarkReaderMain {
     public static void main(String[] args) {
-        String pathToPdf = "assets/omrtest.pdf";
+        String pathToPdf = "assets/omrtest.pdf", pathToFormat = "format.csv";
         System.out.println("Loading pdf at " + pathToPdf);
 
         ArrayList<PImage> pdf = PDFHelper.getPImagesFromPdf(pathToPdf);
         ArrayList<ArrayList<String>> allAnswers = new ArrayList<>();
         ArrayList<ArrayList<String>> allIDs = new ArrayList<>();
+        String[] input = readFormatCSV(pathToFormat).split(" ");
 
         for (PImage page : pdf) {
             DImage image = new DImage(page);
             ArrayList<String> answers = new ArrayList<>();
             ArrayList<String> IDs = new ArrayList<>();
 
-            AnswerFilter filter = new AnswerFilter();
+            AnswerFilter filter = new AnswerFilter(input);
             filter.processImage(image, answers, IDs);
             allAnswers.add(answers);
             allIDs.add(IDs);
@@ -33,12 +37,25 @@ public class OpticalMarkReaderMain {
         outputCSV(allAnswers, allIDs);
     }
 
+    private static String readFormatCSV(String path) {
+        String line = "";
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(path));) {
+            line = br.readLine();
+        } catch (Exception errorObj) {
+            System.err.println("There was a problem reading the " + path);
+        }
+
+        return line;
+    }
+
     private static void outputCSV(ArrayList<ArrayList<String>> allAnswers, ArrayList<ArrayList<String>> allIDs) {
         ArrayList<ArrayList<Boolean>> testGrades = new ArrayList<>();
 
         gradeAnswer(allAnswers, testGrades);
         createCSV(testGrades, allIDs);
         outputItemAnalysis(testGrades);
+
+        System.out.println("\nFinished grading and analysis is complete!");
     }
 
     private static void gradeAnswer(ArrayList<ArrayList<String>> allAnswers, ArrayList<ArrayList<Boolean>> testGrades) {
@@ -91,7 +108,7 @@ public class OpticalMarkReaderMain {
         try {
             PrintWriter out = new PrintWriter(new FileWriter("itemanalysis.csv"));
             for (int i = 0; i < itemAnalysis.length; i++) {
-                if (itemAnalysis[0] != 0)
+                if (itemAnalysis[i] != 0)
                     out.println("Question " + i + " was gotten wrong " + itemAnalysis[i] + " times.");
             }
             out.println("Analysis Complete!");
